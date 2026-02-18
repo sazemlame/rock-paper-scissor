ba5f25a16e55e493f48399dc01b4c2c02a570271
sazemlame <25094541+sazemlame@users.noreply.github.com>
2026-02-18 19:53:14 +0530

    Added UI buttons for Rock, Paper and Scissor

--- rps.js
+++ rps.js
@@ -15,35 +15,62 @@ function getComputerChoice(min,max) {                 //getting the computer's i
 }
 //console.log(getComputerChoice(0,3));
 function getHumanChoice(){     // getting the human input
-    let choice = prompt ("Enter an input between rock,paper and scissor");
-    final_choice = choice.at(0).toUpperCase()+choice.slice(1).toLowerCase();
-    return final_choice;
+    const input = document.querySelector("input");
+    const btns = document.querySelectorAll("button");  //selector for buttons
+    btns.forEach((button) => {
+        button.addEventListener("click", () => {
+            let choice = button.textContent;
+            //console.log(choice);
+            playRound(choice,getComputerChoice(0,3)); //initiating round when button pressed
+            
+            
+
+        });
+        
+    });
+    
     
 }
-//getHumanChoice();
-
+getHumanChoice();
 let humanScore = 0;               // initialising the global score
 let computerScore = 0;
+const container = document.querySelector("#container");
+const div = document.createElement("div");
+div.style.background = "grey";
+const h2 = document.createElement("h2");
+const human= document.createElement("p");
+const computer = document.createElement("p");
+h2.textContent = "Score Table";
+human.textContent = "Human Score : ";
+computer.textContent = "Computer Score : "
+
+div.append(h2);
+div.append(human);
+div.append(computer);
+container.appendChild(div);
 
 function playRound(humanChoice,computerChoice){
+    
+    
     console.log("The choices are ", humanChoice, computerChoice)
     if(humanChoice===computerChoice){
         
     }
     if(humanChoice==="Rock" && computerChoice==="Scissor" || humanChoice === "Paper" && computerChoice === "Rock" || humanChoice === "Scissor" && computerChoice === "Paper"){
         humanScore++;
-        //console.log("Human wins");
+        console.log("Human wins");
 
     }
     if(humanChoice==="Scissor" && computerChoice==="Rock" || humanChoice === "Rock" && computerChoice === "Paper" || humanChoice === "Paper" && computerChoice === "Scissor"){
         computerScore++;
-        //console.log("Computer wins");
+        console.log("Computer wins");
     }
     
 
 
 }
-const rounds=5;
+
+/**const rounds=5;
 function playGame(rounds)
 {
     let round=0;
@@ -62,4 +89,4 @@ function playGame(rounds)
         console.log("It is a tie");
     }
 }
-playGame(rounds);
+playGame(rounds);*/
